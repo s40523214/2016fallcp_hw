ed7f7436d3de2ba352887b11f9d237fc9bb3cf07
s40523214 <40523214@gm.nfu.edu.tw>
2016-12-19 15:27:31 +0800

    Add files via upload

---
+++ blog/tipuesearch_content.js
@@ -0,0 +1,1 @@
+var tipuesearch = {"pages":[{"text":"w14 window.onload=function(){ brython(1); } from browser import document from browser import alert def get_input(ev): the_input= input(\"請輸入\") alert(\"輸入為:\"+str(the_input)) document['ch01'].bind('click',get_input) 取輸入後, 列印出來","tags":"w14","title":"w14","url":"./w14.html"}]};
